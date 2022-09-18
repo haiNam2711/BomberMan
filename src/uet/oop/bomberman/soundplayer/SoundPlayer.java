b71fc298c3dc8502e57639e0ccbfb6c2b56dae90
haiNam2711 <namcuong2711@gmail.com>
2022-09-18 12:05:43 +0700

    Add Sound Track Player.

---
+++ src/uet/oop/bomberman/soundplayer/SoundPlayer.java
@@ -0,0 +1,25 @@
+package uet.oop.bomberman.soundplayer;
+
+import uet.oop.bomberman.BombermanGame;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class SoundPlayer {
+    public static synchronized void playSound(final String soundName) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            BombermanGame.class.getResourceAsStream("/soundtrack/"+soundName+".wav"));
+                    clip.open(inputStream);
+                    clip.start();
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
+    }
+}
