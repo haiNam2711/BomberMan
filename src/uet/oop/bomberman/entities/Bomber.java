d71dc992d2b83022b35f90580a0da47033ed7d11
haiNam2711 <namcuong2711@gmail.com>
2022-09-22 22:43:09 +0700

    Commit code which rounds bomber move when meet obtacles.

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -19,28 +19,25 @@ public class Bomber extends Entity {
     private final int moveLength = Sprite.SCALED_SIZE / 4;
     private final int[] changeX = {0, 0, 1, -1};
     private final int[] changeY = {1, -1, 0, 0};
+    private final Image[] directionImages = {Sprite.player_down.getFxImage(),Sprite.player_up.getFxImage(),Sprite.player_right.getFxImage(),Sprite.player_left.getFxImage()};
 
-//    public Bomber() {
-//        this.x = 1;
-//        this.y = 1;
-//        this.img = Sprite.player_right.getFxImage();
-//    }
     public Bomber(int x, int y, Image img) {
         super(x, y, img);
     }
 
-
+    public Bomber() {
+    }
 
     @Override
     public void update() {
 
     }
 
-    public boolean checkMove() {
-        for (Brick brick : GameMap.bricks) {
+    public boolean checkValidMove() {
 
-            System.out.println(brick.x/32);
-            System.out.println(brick.y/32);
+//        System.out.println(x);
+//        System.out.println(y);
+        for (Brick brick : GameMap.bricks) {
             if (this.checkCollide(brick)) {
                 return false;
             }
@@ -58,34 +55,63 @@ public class Bomber extends Entity {
         return true;
     }
 
-    public void move(int direction) {
-        System.out.println(GameMap.bricks.size());
-        for (int i = 1; i <= moveLength; i++) {
-            x += changeX[direction];
-            y += changeY[direction];
-            boolean flag = checkMove();
-            if (flag == false) {
-                x -= changeX[direction];
-                y -= changeY[direction];
-            }
-        }
+    public void roundAxisMove(int direction) {
 
-        switch (direction) {
-            case 0: {
-                setImg(Sprite.movingSprite(Sprite.player_down, Sprite.player_down_1, Sprite.player_down_2, this.getY(), 60).getFxImage());
-                break;
-            }
-            case 1: {
-                setImg(Sprite.movingSprite(Sprite.player_up, Sprite.player_up_1, Sprite.player_up_2, this.getY(), 60).getFxImage());
-                break;
-            }
-            case 2: {
-                setImg(Sprite.movingSprite(Sprite.player_right, Sprite.player_right_1, Sprite.player_right_2, this.getX(), 60).getFxImage());
-                break;
-            }
-            case 3: {
-                setImg(Sprite.movingSprite(Sprite.player_left, Sprite.player_left_1, Sprite.player_left_2, this.getX(), 60).getFxImage());
-                break;
+        //        changeX = {0, 0, 1, -1};     D-U-R-L
+        //        changeY = {1, -1, 0, 0};     0-1-2-3
+        Bomber newBomber1 = new Bomber();
+        Bomber newBomber2 = new Bomber();
+        if (Math.abs(changeX[direction]) == 0) {
+            newBomber1.setX(this.x + moveLength);
+            newBomber1.setY(this.y + moveLength * changeY[direction]);
+            newBomber2.setX(this.x - moveLength);
+            newBomber2.setY(this.y + moveLength * changeY[direction]);
+        } else {
+            newBomber1.setY(this.y + moveLength);
+            newBomber1.setX(this.x + moveLength * changeX[direction]);
+            newBomber2.setY(this.y - moveLength);
+            newBomber2.setX(this.x + moveLength * changeX[direction]);
+        }
+        if (newBomber1.checkValidMove()) {
+            this.x = newBomber1.x;
+            this.y = newBomber1.y;
+            setImg(directionImages[direction]);
+        } else if (newBomber2.checkValidMove()) {
+            this.x = newBomber2.x;
+            this.y = newBomber2.y;
+            setImg(directionImages[direction]);
+        }
+    }
+
+    public void move(int direction) {
+        x += changeX[direction] * moveLength;
+        y += changeY[direction] * moveLength;
+
+        boolean flag = checkValidMove();
+        if (flag == false) {
+            x -= changeX[direction] * moveLength;
+            y -= changeY[direction] * moveLength;
+            roundAxisMove(direction);
+        } else {
+            switch (direction) {
+                //        changeX = {0, 0, 1, -1};     D-U-R-L
+                //        changeY = {1, -1, 0, 0};     0-1-2-3
+                case 0: {
+                    setImg(Sprite.movingSprite(Sprite.player_down, Sprite.player_down_1, Sprite.player_down_2, this.getY(), 60).getFxImage());
+                    break;
+                }
+                case 1: {
+                    setImg(Sprite.movingSprite(Sprite.player_up, Sprite.player_up_1, Sprite.player_up_2, this.getY(), 60).getFxImage());
+                    break;
+                }
+                case 2: {
+                    setImg(Sprite.movingSprite(Sprite.player_right, Sprite.player_right_1, Sprite.player_right_2, this.getX(), 60).getFxImage());
+                    break;
+                }
+                case 3: {
+                    setImg(Sprite.movingSprite(Sprite.player_left, Sprite.player_left_1, Sprite.player_left_2, this.getX(), 60).getFxImage());
+                    break;
+                }
             }
         }
     }
