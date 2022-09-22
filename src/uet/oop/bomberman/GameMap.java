e0eeba6b367bd1f0a8bd46201d13eaca4e66b5a4
haiNam2711 <namcuong2711@gmail.com>
2022-09-22 17:51:09 +0700

    Refactor Code.

--- src/uet/oop/bomberman/GameMap.java
+++ src/uet/oop/bomberman/GameMap.java
@@ -1,5 +1,18 @@
 package uet.oop.bomberman;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import uet.oop.bomberman.entities.Bomber;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.enemy.Enemy;
+import uet.oop.bomberman.entities.unmovableobject.Brick;
+import uet.oop.bomberman.entities.unmovableobject.Grass;
+import uet.oop.bomberman.entities.unmovableobject.Portal;
+import uet.oop.bomberman.entities.unmovableobject.Wall;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameMap {
     public static final String[] mapLv1 = {
             "###############################",
@@ -16,4 +29,21 @@ public class GameMap {
             "#           *   *  *          #",
             "###############################"
     };
+
+    public static List<Brick> bricks = new ArrayList<>();
+    public static List<Portal> portals = new ArrayList<>();
+    public static List<Wall> walls = new ArrayList<>();
+    public static List<Grass> grasses = new ArrayList<>();
+    public static List<Enemy> enemies = new ArrayList<>();
+    public static Bomber bomberMan ;//= new Bomber();
+
+    public static void render(GraphicsContext gc, Canvas canvas) {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        walls.forEach(g -> g.render(gc));
+        grasses.forEach(g -> g.render(gc));
+        portals.forEach(g -> g.render(gc));
+        bricks.forEach(g -> g.render(gc));
+        enemies.forEach(g -> g.render(gc));
+        bomberMan.render(gc);
+    }
 }
